f903e060ddd3d369ed4a3f3e08cac83777dfba59
Matt Pharr <matt@pharr.org>
2009-11-10 15:28:53 -0800

    Don't use memcpy() in ParamSetItem constructor!  (Not good for T==string!)

--- core/paramset.h
+++ core/paramset.h
@@ -120,7 +120,7 @@ ParamSetItem<T>::ParamSetItem(const string &n, const T *v, int ni) {
     name = n;
     nItems = ni;
     data = new T[nItems];
-    memcpy(data, v, nItems * sizeof(T));
+    for (int i = 0; i < nItems; ++i) data[i] = v[i];
     lookedUp = false;
 }
 
