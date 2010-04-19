86698e042492e5b4bc727a16ef10841167a2ca17
Matt Pharr <matt@pharr.org>
2010-04-19 13:23:52 -0700

    Fix windows build

--- core/progressreporter.cpp
+++ core/progressreporter.cpp
@@ -115,7 +115,7 @@ int TerminalWidth() {
     }
     CONSOLE_SCREEN_BUFFER_INFO bufferInfo = { 0 };
     GetConsoleScreenBufferInfo(h, &bufferInfo);
-    return bufferInfo.dwSize.x;
+    return bufferInfo.dwSize.X;
 #else
     struct winsize w;
     if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) < 0) {
