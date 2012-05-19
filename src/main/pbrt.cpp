36ba1266771c8b1062dd319ec1eda3eb0dcd2f6b
Matt Pharr <matt.pharr@intel.com>
2012-05-18 20:16:15 -0700

    Bump copyright to 2012

--- src/main/pbrt.cpp
+++ src/main/pbrt.cpp
@@ -52,7 +52,7 @@ int main(int argc, char *argv[]) {
     if (!options.quiet) {
         printf("pbrt version %s of %s at %s [Detected %d core(s)]\n",
                PBRT_VERSION, __DATE__, __TIME__, NumSystemCores());
-        printf("Copyright (c)1998-2010 Matt Pharr and Greg Humphreys.\n");
+        printf("Copyright (c)1998-2012 Matt Pharr and Greg Humphreys.\n");
         printf("The source code to pbrt (but *not* the book contents) is covered by the GNU GPL.\n");
         printf("See the file COPYING.txt for the conditions of the license.\n");
         fflush(stdout);
