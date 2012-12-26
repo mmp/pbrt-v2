ee4d1f6619da95fdbe7520fb27881bce5b90875c
Matt Pharr <matt@pharr.org>
2012-12-26 15:42:53 -0800

    Revert "Add missing world to volume transformation in HomogeneousVolumeDensity::tau"
    This reverts commit 73ca02e21b58e870b3e7da3e8451e0736f3036a0.
    
    This transformation is applied in the end by the call to
    HomogeneousVolumeDensity::IntersectP.

--- src/volumes/homogeneous.h
+++ src/volumes/homogeneous.h
@@ -75,8 +75,7 @@ public:
         if (!extent.Inside(WorldToVolume(p))) return 0.;
         return PhaseHG(wi, wo, g);
     }
-    Spectrum tau(const Ray &r, float, float) const {
-        Ray ray = WorldToVolume(r);
+    Spectrum tau(const Ray &ray, float, float) const {
         float t0, t1;
         if (!IntersectP(ray, &t0, &t1)) return 0.;
         return Distance(ray(t0), ray(t1)) * (sig_a + sig_s);
