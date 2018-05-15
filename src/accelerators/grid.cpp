mmp/pbrt-v2

commit da0ae0d6a8fe1fa9f114dde34c56a03e575b21f8

Fix bug in GridAccelerator for rays with zero direction components.
The grid accelerator DDA was carefully written to allow for zero values of
ray direction components, taking advantage of the fact that for x>0,
x/0. == Infinity, and Infinity behaves in reasonable expected ways.

However... IEEE float also has a negative zero value. If we happened to get
a ray with a negative zero component, things would go bad, since we'd
decide that the next t value along that dimension was the one we should
take.

Reported by Mike Phillips.

src/accelerators/grid.cpp
@@ -120,10 +120,11 @@ GridAccel::~GridAccel() {
 }
 
 
-bool GridAccel::Intersect(const Ray &ray, Intersection *isect) const {
+bool GridAccel::Intersect(const Ray &rorig, Intersection *isect) const {
     PBRT_GRID_INTERSECTION_TEST(const_cast<GridAccel *>(this), const_cast<Ray *>(&ray));
     // Check ray against overall grid bounds
     float rayT;
+    Ray ray = rorig;
     if (bounds.Inside(ray(ray.mint)))
         rayT = ray.mint;
     else if (!bounds.IntersectP(ray, &rayT))
@@ -137,6 +138,7 @@ bool GridAccel::Intersect(const Ray &ray, Intersection *isect) const {
     float NextCrossingT[3], DeltaT[3];
     int Step[3], Out[3], Pos[3];
     for (int axis = 0; axis < 3; ++axis) {
+        if (ray.d[axis] == -0.f) ray.d[axis] = 0.f;
         // Compute current voxel for axis
         Pos[axis] = posToVoxel(gridIntersect, axis);
         if (ray.d[axis] >= 0) {
@@ -223,11 +225,12 @@ bool Voxel::Intersect(const Ray &ray, Intersection *isect,
 }
 
 
-bool GridAccel::IntersectP(const Ray &ray) const {
+bool GridAccel::IntersectP(const Ray &rorig) const {
     PBRT_GRID_INTERSECTIONP_TEST(const_cast<GridAccel *>(this), const_cast<Ray *>(&ray));
     RWMutexLock lock(*rwMutex, READ);
     // Check ray against overall grid bounds
     float rayT;
+    Ray ray = rorig;
     if (bounds.Inside(ray(ray.mint)))
         rayT = ray.mint;
     else if (!bounds.IntersectP(ray, &rayT))
@@ -241,6 +244,7 @@ bool GridAccel::IntersectP(const Ray &ray) const {
     float NextCrossingT[3], DeltaT[3];
     int Step[3], Out[3], Pos[3];
     for (int axis = 0; axis < 3; ++axis) {
+        if (ray.d[axis] == -0.f) ray.d[axis] = 0.f;
         // Compute current voxel for axis
         Pos[axis] = posToVoxel(gridIntersect, axis);
         if (ray.d[axis] >= 0) {

commit ab5151b600350db8d5860eb16137f0fa025930fe

Fix dtrace probes in GridAccelerator.
Inadvertently broken in da0ae0d6a8fe1fa9f114dde34c56a03e575b21f8.

via Christian Lessig.

src/accelerators/grid.cpp
@@ -121,7 +121,7 @@ GridAccel::~GridAccel() {
 
 
 bool GridAccel::Intersect(const Ray &rorig, Intersection *isect) const {
-    PBRT_GRID_INTERSECTION_TEST(const_cast<GridAccel *>(this), const_cast<Ray *>(&ray));
+    PBRT_GRID_INTERSECTION_TEST(const_cast<GridAccel *>(this), const_cast<Ray *>(&rorig));
     // Check ray against overall grid bounds
     float rayT;
     Ray ray = rorig;
@@ -226,7 +226,7 @@ bool Voxel::Intersect(const Ray &ray, Intersection *isect,
 
 
 bool GridAccel::IntersectP(const Ray &rorig) const {
-    PBRT_GRID_INTERSECTIONP_TEST(const_cast<GridAccel *>(this), const_cast<Ray *>(&ray));
+    PBRT_GRID_INTERSECTIONP_TEST(const_cast<GridAccel *>(this), const_cast<Ray *>(&rorig));
     RWMutexLock lock(*rwMutex, READ);
     // Check ray against overall grid bounds
     float rayT;